685404d2d926a65830540d23aa5c92c1f619f543
Kiruthika S <105112242+kiruthikasv69@users.noreply.github.com>
2025-07-19 22:35:12 +0530

    Updated start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-set -e
 
-# Pull the Docker image from Docker Hub
-echo
+# Set Docker registry and image info
+DOCKER_REGISTRY_URL="your-registry-url"
+DOCKER_USERNAME="your-username"
+IMAGE_NAME="simple-python-flask-app:latest"
+CONTAINER_NAME="simple-python-flask-app"
 
-# Run the Docker image as a container
-echo
+# Pull the latest image (optional if already pulled)
+echo "Pulling latest image..."
+docker pull $DOCKER_REGISTRY_URL/$DOCKER_USERNAME/$IMAGE_NAME
+
+# Start the container
+echo "Starting container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p 80:5000 \
+  $DOCKER_REGISTRY_URL/$DOCKER_USERNAME/$IMAGE_NAME
